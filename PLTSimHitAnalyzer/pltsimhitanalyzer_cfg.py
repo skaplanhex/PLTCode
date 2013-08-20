16e0597f9d655216c2ba4cedc1aa26f2214f69ce
Steven Kaplan <skaplan@cern.ch>
2013-08-20 12:52:50 -0400

    Updated analyzer for basic output to screen for testing

--- PLTSimHitAnalyzer/pltsimhitanalyzer_cfg.py
+++ PLTSimHitAnalyzer/pltsimhitanalyzer_cfg.py
@@ -7,14 +7,14 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:myfile.root'
-    )
+                            fileNames = cms.untracked.vstring(
+                            'file:/cms/skaplan/PLT/sim/CMSSW_5_3_9/src/SingleElectronPt100_PLTEta_cfi_py_GEN_SIM.root'
+                            )
 )
 
-process.demo = cms.EDAnalyzer('PLTSimHitAnalyzer'
-)
+process.demo = cms.EDAnalyzer('PLTSimHitAnalyzer',
+                              PLTHits = cms.InputTag("g4SimHits","PLTHits","SIM")
+                              )
 
 
 process.p = cms.Path(process.demo)
--- PLTSimHitAnalyzer/src/PLTSimHitAnalyzer.cc
+++ PLTSimHitAnalyzer/src/PLTSimHitAnalyzer.cc
@@ -4,12 +4,12 @@
 // Class:      PLTSimHitAnalyzer
 // 
 /**\class PLTSimHitAnalyzer PLTSimHitAnalyzer.cc PLTCode/PLTSimHitAnalyzer/src/PLTSimHitAnalyzer.cc
-
+ 
  Description: [one line class summary]
-
+ 
  Implementation:
-     [Notes on implementation]
-*/
+ [Notes on implementation]
+ */
 //
 // Original Author:  Steven Kaplan
 //         Created:  Tue Aug 20 10:50:10 EDT 2013
@@ -29,29 +29,38 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+//PSimHitContainer includes PSimHit and Vector already
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+// user defines
+#ifndef SKDEBUG
+#define SKDEBUG
+
 //
 // class declaration
 //
 
 class PLTSimHitAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit PLTSimHitAnalyzer(const edm::ParameterSet&);
-      ~PLTSimHitAnalyzer();
+public:
+    explicit PLTSimHitAnalyzer(const edm::ParameterSet&);
+    ~PLTSimHitAnalyzer();
+    
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+    
+    
+private:
+    virtual void beginJob() ;
+    virtual void analyze(const edm::Event&, const edm::EventSetup&);
+    virtual void endJob() ;
+    
+    virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+    virtual void endRun(edm::Run const&, edm::EventSetup const&);
+    virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+    virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+    
+    // ----------member data ---------------------------
+    edm::InputTag simHitLabel;
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-
-   private:
-      virtual void beginJob() ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
-
-      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-      virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-
-      // ----------member data ---------------------------
+    
 };
 
 //
@@ -68,17 +77,18 @@ class PLTSimHitAnalyzer : public edm::EDAnalyzer {
 PLTSimHitAnalyzer::PLTSimHitAnalyzer(const edm::ParameterSet& iConfig)
 
 {
-   //now do what ever initialization is needed
-
+    //now do what ever initialization is needed
+    simHitLabel = iConfig.getParameter<edm::InputTag>("PLTHits");
+    
 }
 
 
 PLTSimHitAnalyzer::~PLTSimHitAnalyzer()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
+    
+    // do anything here that needs to be done at desctruction time
+    // (e.g. close files, deallocate resources etc.)
+    
 }
 
 
@@ -90,18 +100,19 @@ PLTSimHitAnalyzer::~PLTSimHitAnalyzer()
 void
 PLTSimHitAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
-
-
-
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
+    using namespace edm;
+    
+    Handle<PSimHitContainer> simHitHandle;
+    iEvent.getByLabel(simHitLabel,simHitHandle);
+    
+#ifdef SKDEBUG
+    for (PSimHitContainer::const_iterator iHit = simHitHandle->begin(); iHit != simHitHandle->end(); ++iHit) {
+        std::cout << "Energy Loss: " << iHit->energyLoss() << std::endl;
+        std::cout << "DetID: " << iHit->detUnitId() << std::endl;
+    }
+    
+    
+    
 #endif
 }
 
@@ -145,12 +156,13 @@ PLTSimHitAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSet
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
 PLTSimHitAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
+    //The following says we do not know what parameters are allowed so do no validation
+    // Please change this to state exactly what you do use, even if it is no parameters
+    edm::ParameterSetDescription desc;
+    desc.setUnknown();
+    descriptions.addDefault(desc);
 }
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(PLTSimHitAnalyzer);
+#endif
